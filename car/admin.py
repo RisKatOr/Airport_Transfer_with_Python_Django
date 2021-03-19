4089f5e5c03baa394043badaf356e8ff5a7f0240
aysenurozbay <aysenurozbay1004@gmail.com>
2021-03-19 23:08:25 +0300

    Create image Gallery

--- car/admin.py
+++ car/admin.py
@@ -1,16 +1,31 @@
 from django.contrib import admin
-from car.models import Category, Car
+from car.models import Category, Car, Images
 
 
 # Register your models here.
+
+class CarImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     list_filter = ['status']
     # fields = ['title', 'status']
     # field sadece ekleme silme düzenleme gibi durumlarda dikkate alınacak değişkenleri belirler
 
-admin.site.register(Category,CategoryAdmin)
 class CarAdmin(admin.ModelAdmin):
     list_display = ['title','category', 'status']
     list_filter = ['status','category']
+    inlines =  [CarImageInline]
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title','car']
+    list_filter = ['car']
+
+
+
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Images,ImagesAdmin)
 admin.site.register(Car,CarAdmin)
--- car/models.py
+++ car/models.py
@@ -38,3 +38,11 @@ class Car(models.Model):
         return self.title
 
 
+class Images(models.Model):
+    car= models.ForeignKey(Car, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+    def __str__(self):
+        return self.title
+
+
