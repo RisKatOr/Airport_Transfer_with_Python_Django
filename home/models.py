86eba3ee4c7c566de5db0e6b8b5b5d7d44aca8fb
aysenurozbay <aysenurozbay1004@gmail.com>
2021-03-21 14:15:27 +0300

    Settings

--- home/models.py
+++ home/models.py
@@ -2,3 +2,34 @@ from django.db import models
 
 # Create your models here.
 
+class Setting(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    status = models.CharField(max_length=10, choices=STATUS)
+    title = models.CharField(max_length=150)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    company = models.CharField(max_length=50)
+    address = models.CharField(blank=True,max_length=30)
+    phone = models.CharField(blank=True,max_length=30)
+    fax = models.CharField(blank=True,max_length=30)
+    email = models.CharField(blank=True,max_length=30)
+    smtpserver = models.CharField(blank=True,max_length=20)
+    smtpemail = models.CharField(blank=True,max_length=10)
+    smtppassword = models.CharField(blank=True,max_length=30)
+    smtpport = models.CharField(blank=True,max_length=5)
+    facebook = models.CharField(blank=True,max_length=50)
+    instagram = models.CharField(blank=True,max_length=50)
+    twitter = models.CharField(blank=True,max_length=50)
+    aboutus = models.TextField(blank=True)
+    contact = models.TextField(blank=True)
+    references = models.TextField(blank=True)
+    icon= models.ImageField(blank=True, upload_to='images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
